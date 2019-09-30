1af00e428a09fdd264cd873b24285206d440db28
Susanna Xu <sx563@nyu.edu>
2019-09-30 00:34:39 -0400

    fix spell file and add more tests

--- spell.c
+++ spell.c
@@ -6,44 +6,45 @@
 
 
 bool check_word(const char* word, hashmap_t hashtable[]){
-    //Remove punctuation from beginning and end of word.
-    int begin_index = 0;
-    while(!isalpha(word[begin_index])){begin_index++;}
-    int end_index = strlen(word) - 1;
-    while(!isalpha(word[end_index])){end_index--;}
-    char filtered_word[LENGTH];
-    filtered_word[strlen(word)] = '\0';
-    int j = 0;
-    for(int i = begin_index; i <= end_index; i++){
-        filtered_word[j] = word[i];
-        j++;
+    if(strlen(word) > LENGTH){return false;}
+
+
+    //Check if word is number, if so return true
+    int num_of_digits = 0;
+    for(int i = 0; i < strlen(word); i++){
+        if(isdigit(word[i])){num_of_digits++;}
     }
-    
+    if(num_of_digits == strlen(word)){
+        return true;
+    }
+
+
     //Set int bucket to the output of hash_function(word).
-    int bucket = hash_function(filtered_word);
+    int bucket = hash_function(word);
     //Set hashmap_t cursor equal to hashmap[bucket].
     hashmap_t cursor = hashtable[bucket];
     //While cursor is not NULL:
     while (cursor != NULL){
         //If word equals cursor->word: return True.
-        if (!strcmp(filtered_word, cursor->word)){return true;}
+        if (!strcmp(word, cursor->word)){return true;}
         //Set cursor to cursor->next.
         cursor = cursor->next;
     }
     
+    char lowercase_word[LENGTH];
     //Lowercase word
-    for(int i = 0; i < strlen(filtered_word); i++){
-        filtered_word[i] = tolower(filtered_word[i]);
+    for(int i = 0; i < strlen(word); i++){
+        lowercase_word[i] = tolower(word[i]);
     }
-
+    lowercase_word[strlen(word)] = '\0';
     //Set int bucket to the output of hash_function(word).
-    bucket = hash_function(filtered_word);
+    bucket = hash_function(lowercase_word);
     //Set hashmap_t cursor equal to hashtable[bucket].
     cursor = hashtable[bucket];
     //While cursor is  not NULL:
     while (cursor != NULL){
         //If lower_case(word) equals cursor->word: return True.
-        if (!strcmp(filtered_word, cursor->word)){return true;}
+        if (!strcmp(lowercase_word, cursor->word)){return true;}
         //Set cursor to cursor->next.
         cursor = cursor->next;
     }
@@ -57,25 +58,65 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]){
     //Set int num_misspelled to 0.
     int num_misspelled = 0;
 
-    char line[LENGTH]; 
+    char c;
+    char word[LENGTH];
+    int index = 0;
+    int num_of_non_alpha_char = 0;
+    int num_of_digits = 0;
     //While line in fp is not EOF (end of file):
-    while (fgets(line, sizeof(line), fp) != NULL){
-        //Read the line.
-        //Get each word from line
-        char* word = strtok(line, " \n\r\t,.!?;");
-
-        //For each word in line:
-        while(word != NULL){
-            //If not check_word(word):
-            if(!check_word(word, hashtable)){
-                //Append word to misspelled.
-                misspelled[num_misspelled] = malloc(LENGTH);
-                strncpy(misspelled[num_misspelled], word, LENGTH);
-                //Increment num_misspelled.
-                num_misspelled++;
+    while((c = fgetc(fp)) != EOF){
+        if(strlen(word) == 0){
+            if(!isspace(c)){
+                word[0] = c;
+                index = 1;
+                if(!isalpha(c)){num_of_non_alpha_char++;}
+                if(isdigit(c)){num_of_digits++;}
             }
-            //tokenize same string, to get next word in string
-            word = strtok(NULL," \n\r\t,.!?;");
+        }else{
+            if(isspace(c)){
+                //if word is not a number
+                if(strlen(word) != num_of_digits){
+                    if(num_of_non_alpha_char > 0){
+                        //Remove non alpha from beginning and end of word.
+                        int begin_index = 0;
+                        while(!isalpha(word[begin_index])){begin_index++;}
+                        int end_index = strlen(word) - 1;
+                        while(!isalpha(word[end_index])){end_index--;}
+                        char filtered_word[LENGTH];
+                        int j = 0;
+                        for(int i = begin_index; i <= end_index; i++){
+                            filtered_word[j] = word[i];
+                            j++;
+                        }
+                        filtered_word[j] = '\0';
+                        strncpy(word, filtered_word, LENGTH);
+                        word[j] = '\0';
+                    }else{
+                        word[index] = '\0';
+                    }
+                    //If not check_word(word):
+                    if(!check_word(word, hashtable)){
+                        //Append word to misspelled.
+                        misspelled[num_misspelled] = malloc(LENGTH);
+                        strncpy(misspelled[num_misspelled], word, LENGTH);
+                        //Increment num_misspelled.
+                        num_misspelled++;
+                    }
+                }
+                //clear string
+                int len_of_word = strlen(word);
+                for(int i = 0; i < len_of_word; i++){
+                    word[i] = '\0';
+                }
+                index = 0;
+                num_of_non_alpha_char = 0;
+                num_of_digits = 0;
+            }else{
+                word[index] = c;
+                index++;
+                if(!isalpha(c)){num_of_non_alpha_char++;}
+                if(isdigit(c)){num_of_digits++;}
+            }  
         }
     }
     
@@ -107,8 +148,8 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
         //Set new_node->next to NULL.
         new_node->next = NULL;
         //Set new_node->word equal to word.
-        strncpy(new_node->word, dict_word, sizeof(dict_word));
-
+        strncpy(new_node->word, dict_word, strlen(dict_word));
+        new_node->word[strlen(dict_word)] = '\0';
         //Set int bucket to hash_function(word).
         int bucket = hash_function(new_node->word);
         //if hashtable[bucket] is NULL: 
@@ -129,9 +170,3 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 
     return true;
 }
-
-
-
-
-
-
--- test_main.c
+++ test_main.c
@@ -8,10 +8,26 @@
 START_TEST(test_dictionary_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
-    ck_assert(load_dictionary(TESTDICT, hashtable));
+    load_dictionary(TESTDICT, hashtable);
     // Here we can test if certain words ended up in certain buckets
     // to ensure that our load_dictionary works as intended. I leave
     // this as an exercise.
+    const char* word1 = "first";
+    int bucket1 = hash_function(word1);
+    hashmap_t cursor1 = hashtable[bucket1];
+    ck_assert(!strcmp(cursor1->word, word1));
+    const char* word2 = "second";
+    int bucket2 = hash_function(word2);
+    hashmap_t cursor2 = hashtable[bucket2];
+    ck_assert(!strcmp(cursor2->word, word2));
+    const char* word3 = "third";
+    int bucket3 = hash_function(word3);
+    hashmap_t cursor3 = hashtable[bucket3];
+    ck_assert(!strcmp(cursor3->word, word3));
+    const char* word4 = "test";
+    int bucket4 = hash_function(word4);
+    hashmap_t cursor4 = hashtable[bucket4];
+    ck_assert(!strcmp(cursor4->word, word4));
 }
 END_TEST
 
@@ -24,6 +40,8 @@ START_TEST(test_check_word_normal)
     ck_assert(check_word(correct_word, hashtable));
     ck_assert(!check_word(punctuation_word_2, hashtable));
     // Test here: What if a word begins and ends with "?
+    const char* word_with_question_marks = "?Justice?";
+    ck_assert(!check_word(word_with_question_marks, hashtable));
 }
 END_TEST
 
@@ -49,6 +67,75 @@ START_TEST(test_check_words_normal)
 }
 END_TEST
 
+START_TEST(test_check_word_overflow)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* long_word = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    ck_assert(!check_word(long_word, hashtable));
+}
+END_TEST
+
+START_TEST(test_dictionary_file_does_not_exist)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(!load_dictionary("file_does_not_exit.txt", hashtable)); 
+}
+END_TEST
+
+START_TEST(test_dictionary_file_does_exist)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(load_dictionary(TESTDICT, hashtable));   
+}
+END_TEST
+
+START_TEST(test_check_word_number)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* number = "101";
+    ck_assert(check_word(number, hashtable));
+}
+END_TEST
+
+START_TEST(test_check_words_numbers)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("numbers.txt","w+");
+    for(int i = 0; i < 11; i++){
+       fprintf(fp, "%d\n", i);
+    }
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert(num_misspelled == 0);
+    fclose(fp);
+}
+END_TEST
+
+START_TEST(test_check_words_mixed)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("mixed.txt","w+");
+    for(int i = 0; i < 11; i++){
+        hashmap_t cursor = hashtable[i];
+        int j = i;
+        while(cursor->word == NULL){
+            j++;
+            cursor = hashtable[j];
+        }
+        fprintf(fp, " %d %s ", i, cursor->word);
+    }
+    
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert(num_misspelled == 0);
+    fclose(fp);
+}
+END_TEST
+
 Suite *
 check_word_suite(void)
 {
@@ -57,8 +144,39 @@ check_word_suite(void)
     suite = suite_create("check_word");
     check_word_case = tcase_create("Core");
     tcase_add_test(check_word_case, test_check_word_normal);
-    tcase_add_test(check_word_case, test_check_words_normal);
+    tcase_add_test(check_word_case, test_check_word_overflow);
+    tcase_add_test(check_word_case, test_check_word_number);
     suite_add_tcase(suite, check_word_case);
+
+    return suite;
+}
+
+Suite *
+check_words_suite(void)
+{
+    Suite * suite;
+    TCase * check_words_case;
+    suite = suite_create("check_words");
+    check_words_case = tcase_create("Core");
+    tcase_add_test(check_words_case, test_check_words_normal);
+    tcase_add_test(check_words_case, test_check_words_numbers);
+    tcase_add_test(check_words_case, test_check_words_mixed);
+    suite_add_tcase(suite, check_words_case);
+
+    return suite;
+}
+
+Suite *
+load_dictionary_suite(void)
+{
+    Suite * suite;
+    TCase * load_dictionary_case;
+    suite = suite_create("load_dictionary");
+    load_dictionary_case = tcase_create("Core");
+    tcase_add_test(load_dictionary_case, test_dictionary_normal);
+    tcase_add_test(load_dictionary_case, test_dictionary_file_does_not_exist);
+    tcase_add_test(load_dictionary_case, test_dictionary_file_does_exist);
+    suite_add_tcase(suite, load_dictionary_case);
 
     return suite;
 }
@@ -72,9 +190,17 @@ main(void)
     
     suite = check_word_suite();
     runner = srunner_create(suite);
+
+    suite = check_words_suite();
+    srunner_add_suite(runner, suite);
+
+    suite = load_dictionary_suite();
+    srunner_add_suite(runner, suite);
+
     srunner_run_all(runner, CK_NORMAL);
     failed = srunner_ntests_failed(runner);
     srunner_free(runner);
+
     return (failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
 
